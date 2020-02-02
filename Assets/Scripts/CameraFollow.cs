4ea6ded161a1ab5a071153c8475eb6b184440c87
dubdub39 <pwiley39@gmail.com>
2020-02-02 06:56:40 -0800

    Added enemy player and movement logic

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private Vector3 offset;
+
+    private void Update()
+    {
+        //if we don't have a target object assigned, exit out of this method
+        if (target == null)
+            return;
+        //code to track our player.  a vector3 is assigned to have the same coordinates as our
+        //player, plus an offset amount to create distance between player & camera.
+        //offset.y just keeps the Y axis in a set position assigned by us in the inspector.  We do this in
+        //case we don't want the camera to slightly track upwards with our player whenever he jumps.
+
+        Vector3 newPos = target.position + offset;
+        //newPos.y = offset.y;
+
+        transform.position = newPos;
+    }
+
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;    //speed of enemy movement
+
+    [SerializeField]
+    private Vector3 offsetEndPos;   //the vector3 where the enemy's movement will end
+
+    private Vector3 startPos;   //enemy starting point
+    private Vector3 targetPos;  //idk about this variable yet
+
+    private void Awake()
+    {
+        startPos = transform.position;  //assigns startPos var as the enemy's initial starting point
+        targetPos = startPos + offsetEndPos;    
+    }
+
+    private void Update()
+    {
+        //this code actually moves the enemy.  the MoveTowards method takes 3 parameters in this case:
+        //the current position, the position you'd like to get to, and the speed at which to travel
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        //if the enemy makes it to the target position
+        if (transform.position == targetPos)
+        {   
+            //if the target position is actually the enemy's starting point
+            if (targetPos == startPos)
+                //set the target position back to the end point we'd like the enemy to go back to
+                targetPos = startPos + offsetEndPos;
+                //if we've reached the target position, go back to the starting position.
+            else if (targetPos == startPos + offsetEndPos)
+                targetPos = startPos;
+        }
+    }
+}
+

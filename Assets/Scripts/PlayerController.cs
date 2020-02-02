cffa8376e336d6bc168d430552b8e9528a9cf0de
dubdub39 <pwiley39@gmail.com>
2020-02-02 05:07:33 -0800

    added player movement script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    public float moveSpeed;
+
+    private Rigidbody rig;
+
+    private void Awake()
+    {
+        //get the rigidbody component
+        rig = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        // getting our inputs
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(xInput, 0, zInput) * moveSpeed;
+
+        rig.velocity = dir;
+    }
+}
